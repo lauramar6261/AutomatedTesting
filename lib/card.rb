b17df84880ce69bb999a07b8e5f74e8a546b6f69
Laura <larmartinez2@gmail.com>
2018-08-27 11:21:38 -0700

    to_s returns a readable String value for Ace, Jack, Queen, King and values

--- lib/card.rb
+++ lib/card.rb
@@ -1,5 +1,6 @@
 
 # card.rb
+require 'pry'
 
 class Card
   attr_reader :value, :suit
@@ -12,6 +13,16 @@ class Card
   end
 
   def to_s
+    case @value
+    when 1
+      return "Ace of #{suit.to_s}"
+    when 11
+      return "Jack of #{suit.to_s}"
+    when 12
+      return "Queen of #{suit.to_s}"
+    when 13
+      return "King of #{suit.to_s}"
+    end
     return "#{value} of #{suit.to_s}"
   end
 
--- specs/card_spec.rb
+++ specs/card_spec.rb
@@ -1,5 +1,6 @@
 
 require_relative 'spec_helper'
+require 'pry'
 
 describe Card do
   describe "You can create a Card instance" do
@@ -23,19 +24,35 @@ describe Card do
   end
 
   describe "Testing to_s" do
-
     it "to_s returns a readable String value logically for values 2-10" do
       # Test to ensure that to_s works for cards values 2-10
       # for example:  "2 of diamonds"
+      @my_card = Card.new(2, :clubs)
+      expect(@my_card.to_s).must_equal '2 of clubs'
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
       # Test to ensure that to_s works for cards values 1, and 11-13
       # For example: "Queen of hearts"
+      @my_card = Card.new(1, :diamonds)
+      expect(@my_card.to_s).must_equal 'Ace of diamonds'
+
+      @my_card = Card.new(11, :diamonds)
+      expect(@my_card.to_s).must_equal 'Jack of diamonds'
+
+      @my_card = Card.new(12, :diamonds)
+      expect(@my_card.to_s).must_equal 'Queen of diamonds'
+
+      @my_card = Card.new(13, :diamonds)
+      expect(@my_card.to_s).must_equal 'King of diamonds'
+
     end
   end
 
   describe "Reader methods" do
+    before do
+      @my_card = Card.new(2, :clubs)
+    end
 
     it "Can retrieve the value of the card using a `.value`." do
       # ensure that `.value works as expected`
