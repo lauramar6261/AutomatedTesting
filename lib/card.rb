3309a89c21737d84a2cc7b2f00234c47b288b3d0
Laura <larmartinez2@gmail.com>
2018-08-27 10:28:54 -0700

    Raises an ArgumentError for invalid parameters

--- lib/card.rb
+++ lib/card.rb
@@ -3,16 +3,27 @@
 
 class Card
   attr_reader :value, :suit
-
+  @@suites = [:hearts, :spades, :clubs, :diamonds]
   def initialize(value, suit)
     @value = value
     @suit = suit
-    
+    card_values(@value, @suit)
 
   end
 
   def to_s
     return "#{value} of #{suit.to_s}"
+  end
+
+  def card_values(num, suite)
+    if num > 13 || num <= 0
+      raise ArgumentError
+    end
+
+    unless @@suites.include? suite
+      raise ArgumentError
+    end
+
   end
 
 end
